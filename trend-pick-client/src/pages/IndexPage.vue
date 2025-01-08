13a35dc0f3193f8534c2d67524e52fbe45c446d2
nirey-l <lyr725@gmail.com>
2025-01-08 17:15:53 +0900

    인덱스페이지 컴포넌트 추가

--- trend-pick-client/src/pages/IndexPage.vue
+++ trend-pick-client/src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="q-pa-lg">
+  <div class="q-pa-lg q-mb-xs">
     <q-btn-toggle
       v-model="model"
       toggle-color="white"
@@ -21,7 +21,7 @@
     />
   </div>
 
-  <div class="q-pa-lg" style="width: 1570px;">
+  <div class="q-pa-lg" style="width: 1620px;">
     <div class="row no-wrap shadow-2">
       <q-toolbar class="col-8" :class="$q.dark.isActive ? 'bg-grey-9 text-white' : 'bg-grey-3'">
         <q-btn flat round dense icon="menu" />
@@ -36,7 +36,10 @@
    </div>
   </div>
 
-  <q-page class="q-pa-lg">
+  <div class="q-mt-lg flex flex-center text-bold text-h2">Trend Pick</div>
+  <div class="q-mt-lg flex flex-center">쇼핑몰 소개 문구</div>
+
+  <q-page class="q-pa-lg q-mt-lg">
     <q-carousel
       style="width: 1570px; height: 400px"
       animated
@@ -56,36 +59,27 @@
       <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
     </q-carousel>
 
-    <div class="q-ma-xl">
-      <q-card style="max-width: 350px;" class="my-card" flat bordered>
-        <img src="~assets/1.png">
-        <div class="row justify-between items-center q-pa-md">
-          <div>
-            <div class="text-bold">남성 니트</div>
-            <div>30,000원</div>
+    <div class="q-mt-xl q-mb-lg flex flex-center text-bold text-h5">New Item</div>
+
+    <div class="grid-container">
+      <div class="q-ma-md" v-for="(item, i) in items" :key="i" style="width: 340px;">
+        <q-card style="max-width: 350px;" class="my-card" flat bordered>
+          <img src="~assets/1.png">
+          <div class="row justify-between items-center q-pa-md">
+            <div>
+              <div class="text-bold">{{ item.name }}</div>
+              <div>{{ item.price }}</div>
+            </div>
+             <q-card-actions align="right">
+              <q-btn flat round color="red" icon="favorite" />
+              <q-btn flat round color="primary" icon="share" />
+            </q-card-actions>
           </div>
-        <q-card-actions align="right">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-        </div>
-      </q-card>
+        </q-card>
+      </div>
     </div>
-    <div class="q-ma-xl">
-      <q-card style="max-width: 350px;" class="my-card" flat bordered>
-        <img src="~assets/1.png">
-        <div class="row justify-between items-center q-pa-md">
-          <div>
-            <div class="text-bold">남성 니트</div>
-            <div>30,000원</div>
-          </div>
-        <q-card-actions align="right">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-        </div>
-      </q-card>
-    </div>
+
+    <div class="q-mt-xl q-mb-lg flex flex-center text-bold text-h5">Weekly Best</div>
   </q-page>
 </template>
 
@@ -93,4 +87,22 @@
 import { ref } from "vue";
 const slide = ref(1) //첫 번째 슬라이드가 초기 상태에서 표시
 const autoplay = ref(true) //캐러셀은 초기 상태에서 자동 재생
+const items = ref([
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" },
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" },
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" },
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" },
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" },
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" },
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" },
+  { image: "~assets/1.png", name: "남성 니트", price: "30,000원" }
+])
 </script>
+
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); /* 4열 */
+  gap: 16px; /* 카드 간의 간격 */
+}
+</style>
