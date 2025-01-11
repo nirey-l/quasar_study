3ca581f8b4a082f4d715df396441d434196983aa
nirey-l <lyr725@gmail.com>
2025-01-11 19:14:34 +0900

    디테일 페이지 컴포넌트 추가
    사이즈 버튼 추가

--- trend-pick-client/src/pages/DetailPage.vue
+++ trend-pick-client/src/pages/DetailPage.vue
@@ -15,8 +15,14 @@
 
             <div class="q-mb-xs text-h7 text-bold">사이즈</div>
             <q-btn v-if="isFreesize" class="q-mb-lg" label="Free" type="submit" color="black" />
-            <q-btn v-else class="q-mb-lg" label="size select" type="submit" color="black" />
-
+            <q-btn-group v-else class="q-mb-lg">
+                <q-btn label="XS" type="submit" color="black" />
+                <q-btn label="S" type="submit" color="black" />
+                <q-btn label="M" type="submit" color="black" />
+                <q-btn label="L" type="submit" color="black" />
+                <q-btn label="XL" type="submit" color="black" />
+            </q-btn-group>
+ 
             <div class="q-mb-xs text-h7 text-bold">수량</div>
             <div class="q-pa-md q-mb-lg">
                 <q-input v-model="quantity" type="number" filled />
@@ -36,12 +42,10 @@ const $route = useRoute()
 const slide = ref(1) //첫 번째 슬라이드가 초기 상태에서 표시
 const price = 30000
 const quantity = ref(1)
-const totalPrice = computed(() => price*quantity.value)
+const totalPrice = computed(() => price*quantity.value) //computed(): 이미 ref로 선언된 값에 따라 또 바껴야 하는 변수
 const isFreesize = ref(false)
 
 // axios.get('getProduct?')
-
-//computed(): 이미 ref로 선언된 값에 따라 또 바껴야 하는 변수
 
 onMounted(() => {
     console.log($route.query.itemId)
